5a728c8d306e27d8461ac03f992a7f0611e11ec0
longnhatlekk <longnhatlekk@gmail.com>
2023-06-08 01:30:48 +0700

    user

--- BirdPlatForm/BirdPlatForm/Controllers/UserController.cs
+++ BirdPlatForm/BirdPlatForm/Controllers/UserController.cs
@@ -26,15 +26,15 @@ namespace BirdPlatForm.Controllers
         }
         [HttpPost("Login")]
         public async Task<IActionResult> Login(UserView model)
-        
+
         {
-            var user =  _context.TbUsers.Include(x => x.Role).SingleOrDefault(o => o.Email == model.Email && model.Password == o.Password);
+            var user = _context.TbUsers.Include(x => x.Role).SingleOrDefault(o => o.Email == model.Email && model.Password == o.Password);
             if (user == null) return Ok(new ErrorRespon
             {
                 Error = false,
                 Message = "Email Or Password Incorrect :("
             });
-            var token = await GeneratToken(user);
+            var token = GeneratToken(user);
             return Ok(new ErrorRespon
             {
                 Error = true,
@@ -43,7 +43,7 @@ namespace BirdPlatForm.Controllers
             });
         }
 
-        private async Task<TokenRespon> GeneratToken(TbUser user)
+        private string GeneratToken(TbUser user)
         {
             var jwttokenHandler = new JwtSecurityTokenHandler();
             var secretkey = Encoding.UTF8.GetBytes(_config["JWT:Key"]);
@@ -52,6 +52,7 @@ namespace BirdPlatForm.Controllers
             {
                 Subject = new ClaimsIdentity(new[] {
                     new Claim(ClaimTypes.Name, user.Name),
+                    new Claim("UserId", user.UserId.ToString()),
                     new Claim(JwtRegisteredClaimNames.Email, user.Email),
                     new Claim(JwtRegisteredClaimNames.Sub, user.Email),
                     new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
@@ -60,35 +61,15 @@ namespace BirdPlatForm.Controllers
 
 
                 }),
-                Expires = DateTime.UtcNow.AddDays(1),
+                Expires = DateTime.UtcNow.AddDays(3),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(secretkey), SecurityAlgorithms.HmacSha512)
 
 
             };
-
-
             var token = jwttokenHandler.CreateToken(tokendescription);
             var accessToken = jwttokenHandler.WriteToken(token);
-            var refreshToken = GenerateRefreshToken();
 
-            //Lưu database  
-            var refreshTokenEntity = new TbToken
-            {
-
-                Id = user.UserId,
-                UserId = user.UserId,
-                Token = refreshToken,
-                ExpiryDate = DateTime.UtcNow.AddSeconds(10),
-            };
-
-            await _context.AddAsync(refreshTokenEntity);
-            await _context.SaveChangesAsync();
-
-            return new TokenRespon
-            {
-                AccessToken = accessToken,
-                RefreshToken = refreshToken
-            };
+            return accessToken;
         }
 
         private string GenerateRefreshToken()
@@ -102,16 +83,19 @@ namespace BirdPlatForm.Controllers
                 return Convert.ToBase64String(random);
             }
         }
+
+
+
         [HttpPost("Register")]
         public async Task<ActionResult<TbUser>> Register(RegisterModel register)
         {
-            var regis =  _context.TbUsers.FirstOrDefault(o => o.Email == register.Email);
-            if(regis != null)
+            var regis = _context.TbUsers.FirstOrDefault(o => o.Email == register.Email);
+            if (regis != null)
             {
                 return Ok(new ErrorRespon
                 {
                     Error = false,
-                    Message ="Fail"
+                    Message = "Fail"
                 });
             }
             var user = new TbUser
@@ -137,6 +121,70 @@ namespace BirdPlatForm.Controllers
                 Error = true,
                 Message = "Success"
             });
+
+        }
+        [HttpPut("UpdateUser/{id}")]
+        public async Task<IActionResult> UpdateUser(int id, UserUpdate user)
+
+        {
+            var update = await _context.TbUsers.FindAsync(id);
+            if (update != null)
+            {
+                update.Dob = user.Dob;
+                update.Gender = user.Gender;
+                update.Name = user.Name;
+                update.CreateDate = user.CreateDate;
+                update.UpdateDate = user.UpdateDate;
+                update.Avatar = user.Avatar;
+                update.Phone = user.Phone;
+                update.Address = user.Address;
+                await _context.SaveChangesAsync();
+                return Ok(update);
+            }
+            return BadRequest("Faill ");
+        }
+        
+        [HttpGet("AllUser")]
+        public async Task<IActionResult> GetAllUser()
+        {
+            var user = _context.TbUsers.ToList();
+            return Ok(user);
+        }
+        [HttpGet("GetUser/{id}")]
+        public async Task<IActionResult> GetUserByid(int id)
+        {
+            var user = await _context.TbUsers.FindAsync(id);
+            if (user == null)
+            {
+                return Ok(new ErrorRespon
+                {
+                    Error = false,
+                    Message = "No User :("
+                });
+            }
+            return Ok(user);
+        }
+        [HttpDelete("{Id}")]
+        public async Task<IActionResult> Deleteacount(int Id)
+        {
+            var tokens = _context.TbTokens.Where(t => t.Id == Id).ToList();
+            if (tokens == null)
+            {
+                return null;
+            }
+            // Xóa các bản ghi trong bảng tb_Token
+            _context.TbTokens.RemoveRange(tokens);
+            var user = await _context.TbUsers.FindAsync(Id);
+
+            // Tiếp tục xóa bản ghi trong bảng tb_User
+            if (user != null)
+            {
+                _context.TbUsers.Remove(user);
+            }
+            // Lưu thay đổi vào cơ sở dữ liệu
+            _context.SaveChanges();
+
+            return Ok();
         }
     }
 }
