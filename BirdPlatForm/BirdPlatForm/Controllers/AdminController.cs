01b4c878ed5948769a9b96b3cb5cecf7407b4cad
longnhatlekk <longnhatlekk@gmail.com>
2023-06-08 16:38:23 +0700

    autho

---
+++ BirdPlatForm/BirdPlatForm/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using BirdPlatForm.BirdPlatform;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BirdPlatForm.Controllers
+{
+    [Authorize(Roles = "AD")]
+    [Route("[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly BirdPlatFormContext _context;
+
+        public AdminController(BirdPlatFormContext ct) {
+            _context = ct;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Getall()
+        {
+            var result = _context.TbUsers.ToList();
+            return Ok(result);
+        }
+    }
+}
