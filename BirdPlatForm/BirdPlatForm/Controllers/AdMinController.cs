cf746e053e591184273469da8b2754e9f3452942
longnhatlekk <longnhatlekk@gmail.com>
2023-06-08 16:07:52 +0700

    user

---
+++ BirdPlatForm/BirdPlatForm/Controllers/AdMinController.cs
@@ -0,0 +1,25 @@
+﻿using BirdPlatForm.BirdPlatform;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BirdPlatForm.Controllers
+{
+    [Authorize(Roles = "AD")]
+    [Route("[controller]")]
+    [ApiController]
+    public class AdMinController : ControllerBase
+    {
+        private readonly BirdPlatFormContext _context;
+
+        public AdMinController(BirdPlatFormContext bird)
+        {
+            _context = bird;
+        }
+        [HttpGet]
+        public IEnumerable<TbUser> Get()
+        {
+            return _context.TbUsers.ToList();
+        }
+    }
+}
