be4ed4929fde486fbdb0e2aa1ad7542d6fdfbb36
longnhatlekk <longnhatlekk@gmail.com>
2023-06-08 16:16:22 +0700

    user

--- BirdPlatForm/BirdPlatForm/Controllers/UserController.cs
+++ BirdPlatForm/BirdPlatForm/Controllers/UserController.cs
@@ -138,5 +138,68 @@ namespace BirdPlatForm.Controllers
                 Message = "Success"
             });
         }
+        [HttpPut("UpdateUser/{id}")]
+        public async Task<IActionResult> UpdateUser(int id, UserUpdate user)
+
+        {
+            var update = await _context.TbUsers.FindAsync(id);
+            if (update != null)
+            {
+                update.Dob = user.Dob;
+                update.Gender = user.Gender;
+                update.Name = user.Name;
+                update.CreateDate = user.CreateDate;
+                update.UpdateDate = user.UpdateDate;
+                update.Avatar = user.Avatar;
+                update.Phone = user.Phone;
+                update.Address = user.Address;
+                await _context.SaveChangesAsync();
+                return Ok(update);
+            }
+            return BadRequest("Faill ");
+        }
+
+        [HttpGet("AllUser")]
+        public async Task<IActionResult> GetAllUser()
+        {
+            var user = _context.TbUsers.ToList();
+            return Ok(user);
+        }
+        [HttpGet("GetUser/{id}")]
+        public async Task<IActionResult> GetUserByid(int id)
+        {
+            var user = await _context.TbUsers.FindAsync(id);
+            if (user == null)
+            {
+                return Ok(new ErrorRespon
+                {
+                    Error = false,
+                    Message = "No User :("
+                });
+            }
+            return Ok(user);
+        }
+        [HttpDelete("{Id}")]
+        public async Task<IActionResult> Deleteacount(int Id)
+        {
+            var tokens = _context.TbTokens.Where(t => t.Id == Id).ToList();
+            if (tokens == null)
+            {
+                return null;
+            }
+            // Xóa các bản ghi trong bảng tb_Token
+            _context.TbTokens.RemoveRange(tokens);
+            var user = await _context.TbUsers.FindAsync(Id);
+
+            // Tiếp tục xóa bản ghi trong bảng tb_User
+            if (user != null)
+            {
+                _context.TbUsers.Remove(user);
+            }
+            // Lưu thay đổi vào cơ sở dữ liệu
+            _context.SaveChanges();
+
+            return Ok();
+        }
     }
 }
